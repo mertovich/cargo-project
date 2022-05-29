c4682ec68eda903332b3dfe933a207b37991e86f
mertovich <88425310+mertovich@users.noreply.github.com>
2022-05-29 19:28:29 +0300

    add(Model): Cargo

--- api/model/cargo.go
+++ api/model/cargo.go
@@ -1,9 +1,17 @@
 package model
 
+import "encoding/json"
+
 type Cargo struct {
 	ID       string `json:"id"`
 	Owner    string `json:"owner"`
 	Adress   string `json:"adress"`
 	Location string `json:"location"`
 	Status   string `json:"status"`
+}
+
+func ParseCargo(jsonFile []byte) []Cargo {
+	var cargo []Cargo
+	json.Unmarshal(jsonFile, &cargo)
+	return cargo
 }
