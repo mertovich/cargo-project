4031c7af9a72d1b18e74b72803adc1698c8c100b
mertovich <88425310+mertovich@users.noreply.github.com>
2022-05-31 17:26:53 +0300

    add(web): Router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import NavBar from './ui-library/components/NavBar'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import './assets/styles/App.css'
 
 type Props = {}
 
 const App = (props: Props) => {
   return (
-    <div>App</div>
+    <div className='main-div' >
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+    </div>
   )
 }
 
