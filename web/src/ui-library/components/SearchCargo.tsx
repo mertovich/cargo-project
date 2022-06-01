01d90ab14e132c848b8ce573603d1732b2b5f974
mertovich <88425310+mertovich@users.noreply.github.com>
2022-06-01 08:31:19 +0300

    add(SearchCargo):

--- web/src/ui-library/components/SearchCargo.tsx
+++ web/src/ui-library/components/SearchCargo.tsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../../assets/styles/SearchCargo.css'
 
-type Props = {}
+interface IProps {
+  data: (c: { owner: string, adress: string, location: string, status: string, }) => void
+}
 
-const SearchCargo = (props: Props) => {
+const SearchCargo: React.FC<IProps> = ({ data }) => {
+  const [cargoID, setCargoID] = useState<string>('')
+  const [cargo, setCargo] = useState({ owner: '', adress: '', location: '', status: '', })
+
+  const getCargoID = () => {
+    const requestOptions  = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({ id: cargoID })
+    };
+    fetch('http://localhost:8080/api/getcargo', requestOptions)
+      .then(response => (response.json()))
+      .then(c => data(c));
+  }
+
+  const handleSubmit = () => {
+    getCargoID()
+    data(cargo)
+  }
+
   return (
     <div className='banner-div' >
       <div className='search-div' >
-          <input type='text' placeholder='Cargo Number' className='search-input' />
-          <button className='search-button' >Search</button>
+        <input onChange={(e) => setCargoID(e.target.value)} name='' type='text' placeholder='Cargo Number' className='search-input' />
+        <button onClick={() => handleSubmit()} className='search-button' >Search</button>
       </div>
     </div>
   )
