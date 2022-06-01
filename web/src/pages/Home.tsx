cce590ba747975b8a771af7008902250db54242e
mertovich <88425310+mertovich@users.noreply.github.com>
2022-06-01 08:30:37 +0300

    add(web): home page cargo validation

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CargoInfo from '../ui-library/components/CargoInfo'
 import Greeting from '../ui-library/components/Greeting'
 import SearchCargo from '../ui-library/components/SearchCargo'
 
 type Props = {}
 
 const Home = (props: Props) => {
+  const [Cargo, setCargo] = useState({owner: '',adress: '',location: '',status: '',})
+
+
+  const data = (c:{owner: string,adress: string,location: string,status: string,}) => {
+    setCargo(c)
+  }
+
+  const cargoValidation = ():boolean => {
+    if (Cargo.owner === '' || Cargo.adress === '' || Cargo.location === '' || Cargo.status === '') {
+      return false
+    } else {
+    return true
+    }
+  }
+
   return (
     <div>
-      <SearchCargo />
-      <Greeting />
+      <SearchCargo data={data}/>
+      {cargoValidation() ? <CargoInfo Cargo={Cargo} /> : <Greeting />}
     </div>
   )
 }
