f4e92f7c39973c82762113796181e2aa26fd59b6
mertovich <88425310+mertovich@users.noreply.github.com>
2022-06-01 17:06:26 +0300

    add(desktop): CargoInfo

---
+++ desktop/src/ui-library/components/CargoInfo.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../../assets/styles/CargoInfo.css'
+
+const CargoInfo = props => {
+  return (
+    <div className='cargo-info-div' >
+      <table className='cargo-table-div' >
+        <tr>
+          <th className='cargo-table-th' >owner</th>
+          <th className='cargo-table-th'>adress</th>
+          <th className='cargo-table-th'>location</th>
+          <th className='cargo-table-th'>status</th>
+        </tr>
+        <tr>
+          <td className='cargo-table-td' >{props.Cargo.owner}</td>
+          <td className='cargo-table-td'>{props.Cargo.adress}</td>
+          <td className='cargo-table-td'>{props.Cargo.location}</td>
+          <td className='cargo-table-td'>{props.Cargo.status}</td>
+        </tr>
+      </table>
+    </div>
+  )
+}
+
+export default CargoInfo
