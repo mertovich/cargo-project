e28a02e01db3883d903b6dfbae95f313693c3cc2
mertovich <88425310+mertovich@users.noreply.github.com>
2022-06-02 06:25:39 +0300

    add(desktop): Cargo info add cargo id

--- desktop/src/ui-library/components/CargoInfo.js
+++ desktop/src/ui-library/components/CargoInfo.js
@@ -6,12 +6,14 @@ const CargoInfo = props => {
     <div className='cargo-info-div' >
       <table className='cargo-table-div' >
         <tr>
+          <th className='cargo-table-th'>Cargo ID</th>
           <th className='cargo-table-th' >owner</th>
           <th className='cargo-table-th'>adress</th>
           <th className='cargo-table-th'>location</th>
           <th className='cargo-table-th'>status</th>
         </tr>
         <tr>
+          <td className='cargo-table-td'>{props.Cargo.id}</td>
           <td className='cargo-table-td' >{props.Cargo.owner}</td>
           <td className='cargo-table-td'>{props.Cargo.adress}</td>
           <td className='cargo-table-td'>{props.Cargo.location}</td>
